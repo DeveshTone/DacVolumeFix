b4ed81e0b28d3d244c503466bcb6bf891f2ba53a
DeveshTone <289418952+DeveshTone@users.noreply.github.com>
2026-09-07 15:19:16 +0530

    Remove foojay toolchain resolver to comply with F-Droid build scanner

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,9 +25,6 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
 
 rootProject.name = "DacVolumeFix"
 include(":app")
